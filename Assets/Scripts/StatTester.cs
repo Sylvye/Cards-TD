88b9e6c1073691b78fdccb7129c870acad74fce2
Sylvye <thpexil@gmail.com>
2025-02-11 23:20:52 -0500

    Comit

--- Assets/Scripts/StatTester.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Rendering;
-
-public class StatTester : MonoBehaviour
-{
-    public List<Stat> stats;
-}
